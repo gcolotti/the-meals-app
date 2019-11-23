e80bcb3df4631d1cce088317a9534574e7020209
Guillermo Colotti <guille.theoffspring@gmail.com>
2019-11-22 21:21:54 -0300

    Small modif in screens + Added fonts to assets folder

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const MealDetailScreen = props => {
+const CategoryMealScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Meal Detail Screen!</Text>
+            <Text>The Category Meal Screen!</Text>
         </View>
     );
 }
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default MealDetailScreen;
+export default CategoryMealScreen;
