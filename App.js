a4381e47f915b7155ee8a06839e20ef647806f1d
Guillermo Colotti <guille.theoffspring@gmail.com>
2019-11-27 09:04:32 -0300

    Added Stack Navigator -> MealsNavigator

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+import MealsNavigator from './navigation/MealsNavigator';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'ubuntu-regular': require('./assets/fonts/Ubuntu-Regular.ttf'),
+    'ubuntu-bold': require('./assets/fonts/Ubuntu-Bold.ttf'),
+  })
+}
 
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if(!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}>
+      </AppLoading>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MealsNavigator />
   );
 }
 
@@ -16,4 +37,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 18,
+    fontFamily: 'ubuntu-bold',
+  }
 });
