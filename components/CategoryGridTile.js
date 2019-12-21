9042cae313b887bf5a46d6abb8c4798608f3999d
Guillermo Colotti <guille.theoffspring@gmail.com>
2019-12-21 15:42:04 -0300

    GridTile style fixes

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -37,7 +37,8 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150,
         borderRadius: 10,
-        overflow: 'hidden',
+        overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden' : 'visible',
+        elevation: 8,
     },
     itemContainer: {
         flex: 1,
@@ -46,7 +47,6 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.26,
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 10,
-        elevation: 5,
         justifyContent: 'flex-end',
         alignItems: 'flex-end',
         padding: 15,
