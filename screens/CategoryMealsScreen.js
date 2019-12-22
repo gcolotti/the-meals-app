2edd915fa7909776db2d47f9a91edebce29a8177
Guillermo Colotti <guille.theoffspring@gmail.com>
2019-12-22 15:51:04 -0300

    Refactor MealList to work with both categories & favorites screens

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
 import { CATEGORIES, MEALS } from '../data/dummy-data';
-import MealItem from '../components/MealItem';
+import MealList from './../components/MealList';
 
 const CategoryMealsScreen = props => {
 
@@ -9,34 +8,8 @@ const CategoryMealsScreen = props => {
 
     const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(category_id) >= 0);
 
-    const renderMealItem = itemData => {
-        return (
-            <MealItem
-                title={itemData.item.title}
-                duration={itemData.item.duration}
-                complexity={itemData.item.complexity}
-                affordability={itemData.item.affordability}
-                image={itemData.item.imageUrl}
-                onSelectMeal={() => {
-                    props.navigation.navigate({
-                        routeName: 'MealDetail',
-                        params: {
-                            meal: itemData.item
-                        }
-                    })
-                }}/>
-        );
-    }
-
     return (
-        <View style={styles.screen}>
-            <FlatList
-                data={displayedMeals}
-                keyExtractor={(item, index) => item.id}
-                renderItem={renderMealItem}
-                style={{width: '100%', padding: 10,}}
-                />
-        </View>
+        <MealList listData={displayedMeals} navigation={props.navigation} />
     );
 }
 
@@ -48,13 +21,5 @@ CategoryMealsScreen.navigationOptions = navigationData => {
         headerTitle: selectedCategory.title,
     };
 };
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-});
 
 export default CategoryMealsScreen;
